42551994c69eeefdf917b405aabdff3ebb1e4562
elisa-dep1 <e.rojasreyes@uandresbello.edu>
2024-10-30 05:54:07 -0300

    cambios

--- lib/presentation/screens/secondaryScreens/practice.dart
+++ lib/presentation/screens/secondaryScreens/practice.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class Practice extends StatelessWidget {
-  static const String routeName = 'practice';
+class Practice extends StatefulWidget {
   const Practice({super.key});
+  static const String routeName = 'practice';
+
+  @override
+  PracticeState createState() => PracticeState();
+}
+
+class PracticeState extends State<Practice> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // Inicializa el controlador del WebView
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('assets/juego_prueba/index.html'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Practice'),
+        // ignore: prefer_const_constructors
+        title: Text('Juego en HTML'),
       ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
